2b156df33f0d4823b49f2f31ea8d438e24e995cd
chunfengSun <516108736@qq.com>
2023-01-31 18:29:47 +0800

    Update replay.go

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -126,6 +126,7 @@ func replayCmd(ctx *server.Context, registerAppFlagFn func(cmd *cobra.Command),
 // replayBlock replays blocks from db, if something goes wrong, it will panic with error message.
 func replayBlock(ctx *server.Context, originDataDir string, tmNode *node.Node) {
 	rootDir := ctx.Config.RootDir
+	fmt.Println("fffff", rootDir)
 	dataDir := filepath.Join(rootDir, "data")
 
 	stateStoreDB, err := sdk.NewDB(stateDB, dataDir)
