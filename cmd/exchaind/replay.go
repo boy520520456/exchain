94204440a0b1afaa76b7967a7a9e0e7869a518b6
chunfengSun <516108736@qq.com>
2023-02-07 22:45:51 +0800

    Update replay.go

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -491,7 +491,7 @@ func (m *Manager) cal() {
 	}()
 
 	tt := time.Now()
-	for index := 0; index < 2000; index++ {
+	for index := 0; index < 4000; index++ {
 		go func() {
 			wg.Add(1)
 			for c := range res {
@@ -516,6 +516,9 @@ func (m *Manager) cal() {
 	wg.Wait()
 
 	fmt.Println("guoqi", "all", tmSender.guoqiCnt, "coinTool", tmSender.guoqiCointools, "robotXen", tmSender.guoqiRobotXen)
+	fmt.Println("detail-all", tmSender.allMp)
+	fmt.Println("detail-cointools", tmSender.coinToolsMp)
+	fmt.Println("detail-robotXen", tmSender.robotXenMp)
 
 }
 
@@ -523,7 +526,7 @@ func (m *Manager) cal() {
 func replayBlock(ctx *server.Context, originDataDir string, tmNode *node.Node) {
 
 	//manager := NewManager(originDataDir, 15414660, 17200533)
-	manager := NewManager(originDataDir, 15414660, 16014660)
+	manager := NewManager(originDataDir, 15414660, 15814660)
 
 	ts := manager.GetMaturityTs(common.HexToAddress("0x45b7e4f75d658b5e02811f68fdd71094af03f06e"))
 	time.Unix(ts.Int64(), 0).Year()
