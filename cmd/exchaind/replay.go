66ace96bb04d89e16d53a6c2074396255bd3f28c
chunfengSun <516108736@qq.com>
2023-02-07 15:59:02 +0800

    Update replay.go

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -335,6 +335,9 @@ func (m *Manager) RangeBlock() {
 		go func() {
 			for resp := range res {
 				for ii, v := range resp.resp.DeliverTxs {
+					if len(v.Data) == 0 {
+						continue
+					}
 					data, err := evmtypes.DecodeResultData(v.Data)
 					if err != nil {
 						fmt.Println("fuckkkk", hex.EncodeToString(v.Data), len(v.Data), resp.height, ii)
