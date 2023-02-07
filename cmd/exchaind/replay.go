10c1029ed2d8254aeed4845599970d612c3bd81d
chunfengSun <516108736@qq.com>
2023-02-07 19:06:12 +0800

    Update replay.go

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -508,7 +508,7 @@ func (m *Manager) cal() {
 func replayBlock(ctx *server.Context, originDataDir string, tmNode *node.Node) {
 
 	//manager := NewManager(originDataDir, 15414660, 17200533)
-	manager := NewManager(originDataDir, 15414660, 16114660)
+	manager := NewManager(originDataDir, 15414660, 15750000)
 
 	ts := manager.GetMaturityTs(common.HexToAddress("0x45b7e4f75d658b5e02811f68fdd71094af03f06e"))
 	time.Unix(ts.Int64(), 0).Year()
