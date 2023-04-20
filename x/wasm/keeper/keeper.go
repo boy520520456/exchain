889c70547a7100fcb60a5742408e5fb31f525d94
chunfengSun <516108736@qq.com>
2023-04-20 17:47:06 +0800

    Update keeper.go

--- x/wasm/keeper/keeper.go
+++ x/wasm/keeper/keeper.go
@@ -940,6 +940,9 @@ func (k Keeper) HasContractInfo(ctx sdk.Context, contractAddress sdk.WasmAddress
 func (k Keeper) storeContractInfo(ctx sdk.Context, contractAddress sdk.WasmAddress, contract *types.ContractInfo) {
 	store := k.ada.NewStore(ctx.GasMeter(), ctx.KVStore(k.storeKey), nil)
 	fmt.Println("scf-storecontractInfo", contractAddress, contract.CodeID, reflect.TypeOf(store))
+	if contractAddress.String() == "5A8D648DEE57B2FC90D98DC17FA887159B69638B" {
+		debug.PrintStack()
+	}
 	store.Set(types.GetContractAddressKey(contractAddress), k.cdc.GetProtocMarshal().MustMarshal(contract))
 }
 
