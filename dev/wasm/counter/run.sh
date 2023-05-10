6248ba6e149583bfa67bed48257b849e2c912377
chunfengSun <516108736@qq.com>
2023-05-10 16:03:50 +0800

    Update run.sh

--- dev/wasm/counter/run.sh
+++ dev/wasm/counter/run.sh
@@ -7,8 +7,8 @@ exchaincli tx wasm instantiate 1 '{}' ${OPTIONS}
 
 # ex14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s6fqu27
 # 0xbbE4733d85bc2b90682147779DA49caB38C0aA1F
-exchaincli tx wasm execute 0x5A8D648DEE57b2fc90D98DC17fa887159b69638b '{"add":{"delta":"2"}}'  ${OPTIONS}
+exchaincli tx wasm execute 0x5A8D648DEE57b2fc90D98DC17fa887159b69638b '{"add":{"delta":2}}'  ${OPTIONS}
 
-exchaincli query wasm contract-state smart 0x5A8D648DEE57b2fc90D98DC17fa887159b69638b '{"get_counter":{"delta":"2"}}'
+exchaincli query wasm contract-state smart 0x5A8D648DEE57b2fc90D98DC17fa887159b69638b '{"get_counter":{"delta":2}}'
 exchaincli query wasm contract-state smart 0x5A8D648DEE57b2fc90D98DC17fa887159b69638b '{"get_counter":{"delta":"1"}}'
 
