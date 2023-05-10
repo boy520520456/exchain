79ec81a5baaa144023b57c2510fe7aeb64d0867b
chunfengSun <516108736@qq.com>
2023-05-10 23:19:28 +0800

    Update store.go

--- libs/cosmos-sdk/store/cachekv/store.go
+++ libs/cosmos-sdk/store/cachekv/store.go
@@ -2,8 +2,6 @@ package cachekv
 
 import (
 	"bytes"
-	"encoding/hex"
-	"fmt"
 	"io"
 	"reflect"
 	"sort"
@@ -68,7 +66,7 @@ func (store *Store) GetStoreType() types.StoreType {
 
 // Implements types.KVStore.
 func (store *Store) Get(key []byte) (value []byte) {
-	fmt.Println("fffff", hex.EncodeToString(key), reflect.TypeOf(store.parent))
+	//fmt.Println("fffff", hex.EncodeToString(key), reflect.TypeOf(store.parent))
 	store.mtx.Lock()
 	defer store.mtx.Unlock()
 
